ca9d8be7cc62b8122438804735ebcbfc641fa5cb
Daniel <dzayas79@gmail.com>
2024-12-18 23:25:49 -0500

    basic styling/imports as well as testing font & page visual layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+// logo imports
+import ABC from '@/assets/ABC.svg';
+import CBS from '@/assets/CBS.svg';
+import FOX from '@/assets/FOX.svg';
+import NBC from '@/assets/NBC.svg';
+import { Button } from '@/components/ui/button';
+
+//shadcnui components
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+    <div className='flex flex-col justify-center items-center min-h-screen'>
+      <h1 className='text-2xl font-bold'>
+        Saida & Jermaine's Deal-Menu wip 🛠️
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, facilis
+        ipsa. Et impedit laborum minima nisi esse tenetur dolore ad iste. Illum,
+        tempora!
       </p>
-    </>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, facilis
+        ipsa. Et impedit laborum minima nisi esse tenetur dolore ad iste. Illum,
+        tempora!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, facilis
+        ipsa. Et impedit laborum minima nisi esse tenetur dolore ad iste. Illum,
+        tempora!
+      </p>
+      <Button>sample</Button>
+      {/* map function here to pull from DB and create cards for each property */}
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+          <Button>Buy!</Button>
+        </CardFooter>
+      </Card>
+      {/* 'as seen on' bar */}
+      <div id='tv-bar' className='flex flex-row items-center bg-primary'>
+        <p>As Seen On</p>
+        <img src={ABC} alt='' />
+        <img src={CBS} alt='' />
+        <img src={FOX} alt='' />
+        <img src={NBC} alt='' />
+      </div>
+    </div>
   );
 }
 
 export default App;
+
+// TODO:
+// backend
+// database serving a schema of property details i.e.
+// address, price, arv, etc...
+// state (context API)
+// styling (dark mode toggle!)
+// error handling
+// routing, mostly SPA experience with <Outlet/> use
+// UX: allow buyers to express priority interest in a property (toasts!)
+// borrow navbar from glossary
+// !! auth for Saida & Jermaine
