2de8df9ce160dff42b9d4e9f94ec716048053624
Daniel Zayas <dan@Mac.attlocal.net>
2024-12-21 15:32:45 -0500

    backend in progress, needing DB model & route logic

---
+++ backend/controllers/dealController.js
@@ -0,0 +1,7 @@
+async function getDeals(req, res) {}
+async function getDeal(req, res) {}
+async function addDeal(req, res) {}
+async function updateDeal(req, res) {}
+async function deleteDeal(req, res) {}
+
+export default { getDeals, getDeal, addDeal, updateDeal, deleteDeal };
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-import express from express
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongooose';
+import {
+  getDeals,
+  getDeal,
+  addDeal,
+  updateDeal,
+  deleteDeal,
+} from '@/controllers/dealController';
+
+//middleware
+app.use(express.json());
+
+const app = express();
+
+//routes
+app.get('/', getDeals);
+app.get('/:id', getDeal);
+app.post('/add', addDeal);
+app.put('/update/:id', updateDeal);
+app.delete('/delete/:id', deleteDeal);
+
+app.listen(3000, () => {
+  console.log('live on 3k');
+});
