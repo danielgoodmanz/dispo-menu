4e93148fd3ad8059c8ffbb8b950c70332b87c693
Daniel <dzayas79@gmail.com>
2025-01-02 01:05:40 -0500

    adding error messages to form validation

--- src/components/AddDealForm.tsx
+++ src/components/AddDealForm.tsx
@@ -11,14 +11,23 @@ const AddDealForm = () => {
       .string()
       .min(5, 'Address must be at least 5 chars')
       .max(50, 'Address must not exceed 50 chars'),
-    livingArea: z.string(),
-    lot: z.string(),
+    livingArea: z
+      .string()
+      .min(3, 'Living area must be at least 3 chars')
+      .max(10, 'Living area must not exceed 10 chars'),
+    lot: z
+      .string()
+      .min(3, 'Lot must be at least 3 chars')
+      .max(10, 'Lot must not exceed 10 chars'),
     yearBuilt: z.string().min(4, 'Year must be 4 chars').max(4),
-    escrow: z.string(),
+    escrow: z.string().max(12, 'Escrow must not exceed 12 chars'),
     closing: z.string().max(12, 'Closing must not exceed 12 chars'),
-    price: z.string(),
-    description: z.string(),
-    photo: z.string(),
+    price: z
+      .string()
+      .min(3, 'Price must be at least 3 chars')
+      .max(10, 'Price must not exceed 10 chars'),
+    description: z.string().max(800, "Description can't exceed 800 chars"),
+    photo: z.string().url('Photo must be a valid URL'),
   });
   const {
     register,
@@ -49,7 +58,9 @@ const AddDealForm = () => {
           type='text'
           placeholder='Living area'
         />
+        {errors.address && <p>{errors.address.message}</p>}
         <input {...register('lot')} type='text' placeholder='Lot' />
+        {errors.lot && <p>{errors.lot.message}</p>}
         <input
           {...register('yearBuilt')}
           type='text'
@@ -58,15 +69,21 @@ const AddDealForm = () => {
         {/* errors must go directly after input for respective field */}
         {errors.yearBuilt && <p>{errors.yearBuilt.message}</p>}
         <input {...register('escrow')} type='text' placeholder='Escrow' />
+        {errors.escrow && <p>{errors.escrow.message}</p>}
         <input {...register('closing')} type='text' placeholder='Closing' />
+        {errors.closing && <p>{errors.closing.message}</p>}
         <input {...register('price')} type='text' placeholder='Price' />
+        {errors.price && <p>{errors.price.message}</p>}
         <input
           {...register('description')}
           type='text'
           placeholder='Description'
         />
+        {errors.description && <p>{errors.description.message}</p>}
         <input {...register('photo')} type='text' placeholder='Photo link' />
+        {errors.photo && <p>{errors.photo.message}</p>}
         <Button type='submit'>send</Button>
+        {/* look into Object.keys() here as a way to contain all errors in one place */}
       </form>
     </div>
   );
