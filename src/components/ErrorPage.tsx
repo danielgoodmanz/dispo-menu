dbedb4af4a7716f1b1fe9f702800f69c6856ebf1
Daniel <dzayas79@gmail.com>
2025-01-02 21:58:59 -0500

    back on rr7, using a raw error page instead of useRouteError hook as it requires a data router

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,24 +1,20 @@
 import Header from '@/components/Header';
 import { Button } from '@/components/ui/button';
-import { useRouteError } from 'react-router';
+import { Link } from 'react-router';
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-
   return (
     <div
       id='error-page'
       className='flex flex-col items-center justify-center min-h-screen space-y-4'
     >
       <Header title={'Oops!'} />
-      <p>Sorry, page not found!</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-      <Button variant='ghost' className='text-2xl'>
-        Take me back ✋
-      </Button>
+      <p>Sorry, page not found</p>
+      <Link to={'/'}>
+        <Button variant='ghost' className='text-2xl'>
+          Take me back ✋
+        </Button>
+      </Link>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 import './index.css';
 import App from './App.tsx';
 import AddDealForm from '@/components/AddDealForm.tsx';
 import ErrorPage from '@/components/ErrorPage.tsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/dealform',
-        element: <AddDealForm />,
-      },
-    ],
-  },
-  // TODO: create a landing page
-  {
-    path: '/landing',
-  },
-]);
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='dealform' element={<AddDealForm />} />
+        </Route>
+        // TODO: add a landing page
+        <Route path='landing' />
+        <Route path='/*' element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
