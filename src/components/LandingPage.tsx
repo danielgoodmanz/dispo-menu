0218ab6c231b2d04dc58847182665f567134bdc3
Daniel Zayas <dan@Mac.attlocal.net>
2025-01-25 19:35:12 -0500

    landing page working on image showcase

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,24 +1,34 @@
 import Header from '@/components/Header';
 import NavBar from '@/components/Navbar';
 import card from '@/assets/card.png';
+import interest from '@/assets/interest.png';
 
 const LandingPage = () => {
   return (
     <div>
       <NavBar />
-      <div id='gradient' className='flex flex-col h-screen justify-center'>
-        <Header
-          title={`Florida's most trusted wholesaler for over 20 years`}
-          className='text-6xl text-balance'
-        />
-        <aside className='text-xl '>
-          Request access to our exclusive deal menu from our dispo team
-        </aside>
-        <img
-          src={card}
-          alt='card preview'
-          className='h-[300px] w-[225px] rounded-xl'
-        />
+      <div id='gradient' className='flex h-screen items-center justify-around'>
+        <div className='max-w-[50%] space-y-4'>
+          <Header
+            title={`Florida's most trusted wholesaler for over 20 years`}
+            className='text-6xl text-balance'
+          />
+          <aside className='text-xl max-w-[500px]'>
+            Request access to the exclusive deal menu from our dispo team
+          </aside>
+        </div>
+        <div>
+          <img
+            src={card}
+            alt='card preview'
+            className='h-[300px] w-[300px] rounded-xl shadow-xl shadow-black/25'
+          />
+          <img
+            src={interest}
+            alt='card preview'
+            className='h-[300px] w-[300px] rounded-xl shadow-xl shadow-black/25'
+          />
+        </div>
       </div>
     </div>
   );
