dcd5cd5c05dc8c2e532dabcca0b835837d637855
Daniel Zayas <dan@Mac.attlocal.net>
2025-02-07 15:44:52 -0500

    undo mask on landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -21,11 +21,7 @@ const LandingPage = () => {
         className='h-screen relative'
       >
         {/* TODO: try an img element, apply a maskimg linear gradient instead of our current solution */}
-        <img
-          className='absolute h-full w-full -z-10 mask-linear'
-          src={city}
-          alt=''
-        />
+        <img className='absolute h-full w-full -z-10' src={city} alt='' />
         <div className='flex items-center justify-around pt-[10vh]'>
           <div className='max-w-[50%] space-y-4'>
             <HeroHeader>
