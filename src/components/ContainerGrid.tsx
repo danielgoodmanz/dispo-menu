dd1eaa01cc7eb5703749618467b62c1b2beb3e37
Daniel Zayas <dzayas79@gmail.com>
2025-03-18 21:19:01 -0400

    added responsive breakpoint for lg screens

--- src/components/ContainerGrid.tsx
+++ src/components/ContainerGrid.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'types/appTypes';
 
 const ContainerGrid = ({ children }: AppProps) => {
   return (
-    <div className='grid max-sm:grid-cols-1 grid-cols-3 gap-4 my-12 justify-items-center'>
+    <div className='grid max-lg:grid-cols-2 max-sm:grid-cols-1 grid-cols-3 gap-4 my-12 justify-items-center'>
       {children}
     </div>
   );
